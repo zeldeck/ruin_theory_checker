2f936c9a966c007e203f81d3288023d61820607a
Artem Markov <zeldeck@gmail.com>
2026-06-04 20:52:47 +0300

    Initial commit

---
+++ ruin_coin.py
@@ -0,0 +1,20 @@
+import random
+def ruin_coin(): 
+   print('Привет. Проверям теорию о том, что сколько бы не играл, всё равно проираешь.')
+   coin_start = int(input('Сколько монет ты хочешь вложить на старте? '))
+   
+   steps=0
+
+   while True:
+    steps+=1
+    x = random.randint(0, 1)
+    if x ==1:
+       coin_start+=1
+    else:
+        coin_start-=1
+
+    if coin_start==0:
+       print(f'Игра окончена. Ты всё потерял за {steps} попыток.')
+       break
+   
+ruin_coin()
